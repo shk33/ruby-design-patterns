562c3209d32adb10130a89ce500a01956dffff46
Miguel Coronel <m.coronel.seg@gmail.com>
2015-11-08 13:24:20 -0600

    Change new template pattern

--- template_pattern/report.rb
+++ template_pattern/report.rb
@@ -19,26 +19,22 @@ class Report
   end
 
   def output_start
-    raise 'Called abstract method: output_start'
   end
 
   def output_head
-    raise 'Called abstract method: output_head'
+    output_line @title
   end
-  
+
   def output_body_start
-    raise 'Called abstract method: output_body_start'
   end
-  
+
   def output_line(line)
     raise 'Called abstract method: output_line'
   end
-  
+
   def output_body_end
-    raise 'Called abstract method: output_body_end'
   end
-  
+
   def output_end
-    raise 'Called abstract method: output_end'
   end
 end
